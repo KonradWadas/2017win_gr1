e79fc45d2b4f7245bab016b049bbf67d0f46ffb1
Konrad Wadas <3wadas@taurus.fis.agh.edu.pl>
2017-10-18 19:31:58 +0200

    fix

--- kol1.py
+++ kol1.py
@@ -1,13 +1,39 @@
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can thing of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#Do your best, show off with good, clean, well structured code - this is more important than number of features.
-#After you finish, be sure to UPLOAD this (add, commit, push) to the remote repository.
-#Good Luck
+#!/usr/bin/python
+
+class CustomerAccount:
+	def __init__(self,name,balance):
+		self.name=name
+		self.balance=balance
+	def withdraw(self,amount):
+		self.balance+=amount
+	def deposit(self,amount):
+		self.balance+=amount
+	def getBalance(self):
+		return self.balance
+class Bank:
+	def __init__(self):
+		self.customers= {}
+	def addCustomer(self,name,balance):
+		self.customers[name]=CustomerAccount(name,balance)
+	def withdraw(self,customerName,ammount):
+		self.customers[customerName].withdraw(ammount)
+	def deposit(self,customerName,ammount):
+		self.customers[customerName].deposit(ammount)
+	def transfer(self,fromName,toName,ammount):
+		self.withdraw(fromName,ammount)
+		self.deposit(toName,ammount)
+	def getBalance(self,customerName):
+		return self.customers[customerName].getBalance()
+
+newBank=Bank()
+newBank.addCustomer("janek",1000)
+newBank.addCustomer("marek",0)
+print "Janek's balance:"+ str(newBank.getBalance("janek"))
+print "Marek's balance:"+ str(newBank.getBalance("marek"))
+newBank.withdraw("janek",200)
+newBank.deposit("marek",50)
+print "Janek's balance:"+ str(newBank.getBalance("janek"))
+print "Marek's balance:"+ str(newBank.getBalance("marek"))
+newBank.transfer("janek","marek",200)
+print "Janek's balance:"+ str(newBank.getBalance("janek"))
+print "Marek's balance:"+ str(newBank.getBalance("marek"))
